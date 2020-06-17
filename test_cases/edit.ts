8f7819bbffda7b20cdf1c1bcfffe9b3edd831b39
ccw630 <ccw630@gmail.com>
2020-06-17 18:47:40 +0800

    init

---
+++ test_cases/edit.ts
@@ -0,0 +1,14 @@
+import { Selector } from 'testcafe'
+
+fixture('Autocomplete')
+    .page('http://localhost:3000')
+
+test('Python', async t => {
+    await t.click('#languages').click('#language-Python3')
+    await t.click('#reset')
+    await t.click('.view-line')
+    await t.pressKey('end')
+    for (let i = 0; i < 21; i++) await t.pressKey('backspace')
+    await t.pressKey('i m p')
+    await t.expect(Selector('.contents > .main').withText('import').exists).ok()
+})
