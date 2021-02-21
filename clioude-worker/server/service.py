4fc84e87d3e2ccf727305c8631f66aea03da8e33
ccw630 <ccw630@gmail.com>
2021-02-21 22:51:33 +0800

    fix hostname use on k8s

--- clioude-worker/server/service.py
+++ clioude-worker/server/service.py
@@ -10,7 +10,7 @@ from utils import server_info, logger, token
 
 class WorkerService:
     def __init__(self):
-        self.service_url = f"ws://{socket.gethostname()}:8080"
+        self.service_url = f"ws://{socket.gethostbyname(socket.gethostname())}:8080"
         self.backend_url = os.environ["BACKEND_URL"]
 
     def _request(self, data):
