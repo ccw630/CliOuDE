2e97112206f07d1babf47f005fc068f0860bd962
ccw630 <ccw630@gmail.com>
2021-03-02 00:37:03 +0800

    move kernel out from worker

--- clioude-kernel/src/rules/seccomp_rules.h
+++ clioude-kernel/src/rules/seccomp_rules.h
@@ -6,6 +6,5 @@
 int _c_cpp_seccomp_rules(struct config *_config, bool allow_write_file);
 int c_cpp_seccomp_rules(struct config *_config);
 int general_seccomp_rules(struct config *_config);
-int c_cpp_file_io_seccomp_rules(struct config *_config);
 
 #endif //KERNEL_SECCOMP_RULES_H
